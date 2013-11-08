b7a3e56836600e723c4642ebf1e674988e4efc85
Leslie H. Watter <leslieh@celepar.pr.gov.br>
2013-11-08 17:58:11 -0200

    corrigindo chamada de onClick do min1y e incluindo protótipos de implementações

--- datepicker/src/br/gov/pr/datepicker/DateActivity.java
+++ datepicker/src/br/gov/pr/datepicker/DateActivity.java
@@ -2,10 +2,12 @@ package br.gov.pr.datepicker;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class DateActivity extends Activity {
 
@@ -13,6 +15,16 @@ public class DateActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_date);
+
+		/*
+		// coloca o foco no 1o botão de dia  
+		Button button = (Button)findViewById(R.id.button_d1);
+        button.setFocusable(true);
+        button.setFocusableInTouchMode(true);///add this line
+        button.requestFocus();
+        
+        */
+
 	}
 
 	@Override
@@ -22,12 +34,70 @@ public class DateActivity extends Activity {
 		return true;
 	}
 
+	public void exit(View v){
+		
+	}
+
+	
+	public void onClickButton1(View v){
+		Button botao = (Button) findViewById(R.id.button_d2);
+		botao.setText("7");
+	
+	}
+
+	
+	public void onClickButton2(View v){
+		Context context = getApplicationContext();
+		CharSequence text = "1";
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+	}
+	
+	public void onClickButton3(View v){
+	}
+
+	public void onClickButton4(View v){
+	}
+
+	public void onClickButton5(View v){
+	}
+
+	public void onClickButton6(View v){
+	}
+
 	public void onClickButton7(View v){
 		Button botao = (Button) findViewById(R.id.button_d2);
 		botao.setText("7");
-
-		EditText edtd1 = (EditText) findViewById(R.id.editText_d1);
-		edtd1.setText("7");
-
+	
 	}
+	
+	public void onClickButton8(View v){
+	}
+
+	public void onClickButton9(View v){
+	}
+	
+	public void onClickButton0(View v){
+	}
+
+	public void onClickButtonMin1d(View v){
+	}
+
+	public void onClickButtonMin1m(View v){
+	}
+	
+	public void onClickButtonMin1y(View v){
+	}
+	
+	public void onClickButtonPlus1d(View v){
+	}
+
+	public void onClickButtonPlus1m(View v){
+	}
+	
+	public void onClickButtonPlus1y(View v){
+	}
+
+
 }
